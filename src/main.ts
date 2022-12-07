4fd734074f14bd514da894a16c2a3196bb1b2860
jaeyoung.choi <lactrious@gmail.com>
2022-12-08 08:24:33 +0900

    Bind all hosts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import routes from "./handlers/routes";
 import server from "./infra/server";
 import { serverPort } from "./infra/config";
 
-server.register(routes).listen({ port: serverPort }, (error, address) => {
-  if (error) {
-    server.log.error({ error }, "Cannot start server");
-    process.exit(1);
-  }
-  server.log.info({ address, serverPort }, "Server listening");
-  server.ready().then(() => {
-    server.log.info({}, "Swagger is ready");
+server
+  .register(routes)
+  .listen({ host: "0.0.0.0", port: serverPort }, (error, address) => {
+    if (error) {
+      server.log.error({ error }, "Cannot start server");
+      process.exit(1);
+    }
+    server.log.info({ address, serverPort }, "Server listening");
+    server.ready().then(() => {
+      server.log.info({}, "Swagger is ready");
+    });
   });
-});
